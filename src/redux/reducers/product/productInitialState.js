08f83da4dc8960f3822b2a9bd0bc25860c85e64a
sadikhossain69 <sadikhossain433@gmail.com>
2022-12-05 02:50:16 +0600

    action types and action creators

--- src/redux/reducers/product/productInitialState.js
+++ src/redux/reducers/product/productInitialState.js
@@ -1,1 +1,3 @@
-export const productInitialState = {}
+export const productInitialState = {
+    cart: []
+}
--- src/redux/reducers/product/productReducers.js
+++ src/redux/reducers/product/productReducers.js
@@ -1,5 +1,18 @@
+import { ADD_TO_CART, REMOVE_FROM_CART } from "../../actionTypes/actionTypes";
 import { productInitialState } from "./productInitialState";
 
 export const productReducer = (state = productInitialState, action) => {
-    return state
+    switch (action.type) {
+        case ADD_TO_CART:
+            return {
+                ...state,
+                cart: [...state.cart, action.payload]
+            }
+        case REMOVE_FROM_CART:
+            return {
+
+            }
+        default:
+            return state
+    }
 }
