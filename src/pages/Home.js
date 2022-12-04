ef0e2ae702153049c3e95e8244fc0ef922547b87
sadikhossain69 <sadikhossain433@gmail.com>
2022-12-05 01:46:39 +0600

    product shows in web page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,20 @@
 import React from "react";
+import ProductCard from "../components/ProductCard";
 
 const Home = () => {
+
+  const [products, setProducts] = React.useState([])
+
+  React.useEffect(() => {
+    fetch('products.json').then(res => res.json()).then(data => setProducts(data))
+  }, [])
+
+
   return (
     <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 max-w-7xl gap-14 mx-auto my-10'>
-      <h1>This is home page</h1>
+      {
+        products?.map((product, index) => <ProductCard key={index} product={product} />)
+      }
     </div>
   );
 };
