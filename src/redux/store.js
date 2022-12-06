b84a261fa038b2b3ac08ed5dbe0c34a9eb7d6498
sadikhossain69 <sadikhossain433@gmail.com>
2022-12-06 18:02:36 +0600

    redux devtool extension install and setup

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,5 @@
 import { createStore } from "redux";
 import { productReducer } from "./reducers/product/productReducers";
+import { composeWithDevTools } from 'redux-devtools-extension';
 
-export const store = createStore(productReducer)
+export const store = createStore(productReducer, composeWithDevTools())
