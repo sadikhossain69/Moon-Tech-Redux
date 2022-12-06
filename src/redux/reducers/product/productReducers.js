fae77416cc54432039c36058b282518bdd78edb3
sadikhossain69 <sadikhossain433@gmail.com>
2022-12-06 22:57:44 +0600

    handling complex logic inside reducer part2-

--- src/redux/reducers/product/productReducers.js
+++ src/redux/reducers/product/productReducers.js
@@ -9,12 +9,19 @@ export const productReducer = (state = productInitialState, action) => {
 
     switch (action.type) {
         case ADD_TO_CART:
-            if(selectedProduct) {
-                return state
+            if (selectedProduct) {
+
+                const newCart = state.cart.filter(product => product._id !== selectedProduct._id)
+                selectedProduct.quantity = selectedProduct.quantity + 1
+
+                return {
+                    ...state,
+                    cart: [...newCart, selectedProduct]
+                }
             }
             return {
                 ...state,
-                cart: [...state.cart, {...action.payload, quantity: 1}]
+                cart: [...state.cart, { ...action.payload, quantity: 1 }]
             }
         case REMOVE_FROM_CART:
             return {
