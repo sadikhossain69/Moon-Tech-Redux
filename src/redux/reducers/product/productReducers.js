3481a7df88f5ea4521beb3f7af8d958de4a02478
sadikhossain69 <sadikhossain433@gmail.com>
2022-12-06 19:22:03 +0600

    handling complex login inside reducer part 1

--- src/redux/reducers/product/productReducers.js
+++ src/redux/reducers/product/productReducers.js
@@ -2,16 +2,24 @@ import { ADD_TO_CART, REMOVE_FROM_CART } from "../../actionTypes/actionTypes";
 import { productInitialState } from "./productInitialState";
 
 export const productReducer = (state = productInitialState, action) => {
+
+    const selectedProduct = state.cart.find(product => product._id === action.payload._id)
+
+    console.log(selectedProduct)
+
     switch (action.type) {
         case ADD_TO_CART:
+            if(selectedProduct) {
+                return state
+            }
             return {
                 ...state,
-                cart: [...state.cart, action.payload]
+                cart: [...state.cart, {...action.payload, quantity: 1}]
             }
         case REMOVE_FROM_CART:
             return {
                 ...state,
-                cart : state.cart.filter(product => product._id !== action.payload._id)
+                cart: state.cart.filter(product => product._id !== action.payload._id)
             }
         default:
             return state
