ebc443a297fb69073a4576026b19c4f7a0a51e01
sadikhossain69 <sadikhossain433@gmail.com>
2022-12-06 23:21:45 +0600

    handling complex login inside reducer

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -10,7 +10,7 @@ const Cart = () => {
   return (
     <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 max-w-7xl gap-14 mx-auto my-10'>
       {
-        carts.map((cart, index) => <ProductCard key={index} product={cart} />)
+        carts.sort((a, b) => a._id - b._id).map((cart, index) => <ProductCard key={index} product={cart} />)
       }
     </div>
   );
--- src/redux/reducers/product/productReducers.js
+++ src/redux/reducers/product/productReducers.js
@@ -24,6 +24,18 @@ export const productReducer = (state = productInitialState, action) => {
                 cart: [...state.cart, { ...action.payload, quantity: 1 }]
             }
         case REMOVE_FROM_CART:
+
+            if (selectedProduct.quantity > 1) {
+
+                const newCart = state.cart.filter(product => product._id !== selectedProduct._id)
+                selectedProduct.quantity = selectedProduct.quantity - 1
+
+                return {
+                    ...state,
+                    cart: [...newCart, selectedProduct]
+                }
+            }
+
             return {
                 ...state,
                 cart: state.cart.filter(product => product._id !== action.payload._id)
